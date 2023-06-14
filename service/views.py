8f5f426d3311d96c25f22a734b7e5c9ff07b155b
name <ksyusha0kubay@gmail.com>
2023-06-14 09:10:47 +0300

    Change search API

--- service/views.py
+++ service/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Contact
@@ -7,6 +8,10 @@ from .serializers import ContactSerializer
 class ContactSearchView(APIView):
     def get(self, request):
         query = request.query_params.get("query", "")
-        contacts = Contact.objects.filter(full_name__icontains=query)
+        contacts = Contact.objects.filter(
+            Q(email__icontains=query)
+            | Q(first_name__icontains=query)
+            | Q(last_name__icontains=query)
+        )
         serializer = ContactSerializer(contacts, many=True)
         return Response(serializer.data)
